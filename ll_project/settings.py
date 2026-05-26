08f61f7478be60ce4663f2ae5941a004b456b686
adamenas <pires.a.tom@gmail.com>
2026-05-26 20:19:05 -0300

    voltando database pro postgresql

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -81,12 +81,11 @@ WSGI_APPLICATION = 'll_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
